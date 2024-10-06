fff53b915315aace787b185ecf5a19e20f827853
MoinulAx <moinulkhan347@gmail.com>
2024-10-06 09:33:47 -0400

    Sign Up Err Fixed

--- app/screens/SignUp.jsx
+++ app/screens/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, router } from 'expo-router';
-import { Text, View, TextInput, TouchableOpacity, SafeAreaView, StyleSheet, Image, KeyboardAvoidingView, Platform, ScrollView, Pressable } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, SafeAreaView, StyleSheet, Image, KeyboardAvoidingView, Platform, ScrollView, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import colors from '../config/colors';
 
 const SignUp = () => {
@@ -39,105 +39,113 @@ const SignUp = () => {
     };
 
     return (
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <ScrollView>
-                <SafeAreaView>
-                    <View style={styles.container}>
-                        <View style={styles.backButton}>
-                            <Pressable onPress={handleBackArrow}>
-                                <Image source={require('../assets/back-arrow.png')} style={styles.backButton} />
-                            </Pressable>
-                        </View>
-                        <View style={styles.header}>
-                            <Image source={require('../assets/profile-img.png')} style={styles.profile} />
-                            <Text style={styles.title}>User Details</Text>
-                        </View>
-                        <View style={styles.formContainer}>
-                            <Text>First Name</Text>
-                            <TextInput
-                                placeholder='First Name'
-                                style={styles.inputControl}
-                                value={form.first_name}
-                                onChangeText={first_name => setForm({ ...form, first_name })}
-                                autoCorrect={false}
-                                keyboardType='default'
-                            />
-                            <Text>Last Name</Text>
-                            <TextInput
-                                placeholder='Last Name'
-                                style={styles.inputControl}
-                                value={form.last_name}
-                                onChangeText={last_name => setForm({ ...form, last_name })}
-                                autoCorrect={false}
-                                keyboardType='default'
-                            />
-                            <Text>E-mail</Text>
-                            <TextInput
-                                placeholder='E-mail'
-                                style={styles.inputControl}
-                                value={form.email}
-                                onChangeText={email => setForm({ ...form, email })}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                keyboardType='email-address'
-                            />
-                            <Text>Username</Text>
-                            <TextInput
-                                placeholder='Username'
-                                style={styles.inputControl}
-                                value={form.username}
-                                onChangeText={username => setForm({ ...form, username })}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                keyboardType='default'
-                            />
-                            <Text>Password</Text>
-                            <TextInput
-                                placeholder='Password'
-                                style={styles.inputControl}
-                                value={form.password}
-                                onChangeText={password => setForm({ ...form, password })}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                keyboardType='default'
-                                secureTextEntry={true}
-                            />
-                            <Text>Verify Password</Text>
-                            <TextInput
-                                placeholder='Verify Password'
-                                style={styles.inputControl}
-                                value={form.verify_password}
-                                onChangeText={handleVerifyPasswordChange}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                keyboardType='default'
-                                secureTextEntry={true}
-                            />
-                        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <KeyboardAvoidingView
+                    style={{ flex: 1 }}
+                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                    keyboardVerticalOffset={100}r
+                >
+                    <ScrollView contentContainerStyle={styles.scrollContainer}>
+                        <View style={styles.container}>
+                            <View style={styles.backButton}>
+                                <Pressable onPress={handleBackArrow}>
+                                    <Image source={require('../assets/back-arrow.png')} style={styles.backButton} />
+                                </Pressable>
+                            </View>
+                            <View style={styles.header}>
+                                <Image source={require('../assets/profile-img.png')} style={styles.profile} />
+                                <Text style={styles.title}>User Details</Text>
+                            </View>
+                            <View style={styles.formContainer}>
+                                <Text>First Name</Text>
+                                <TextInput
+                                    placeholder='First Name'
+                                    style={styles.inputControl}
+                                    value={form.first_name}
+                                    onChangeText={first_name => setForm({ ...form, first_name })}
+                                    autoCorrect={false}
+                                    keyboardType='default'
+                                />
+                                <Text>Last Name</Text>
+                                <TextInput
+                                    placeholder='Last Name'
+                                    style={styles.inputControl}
+                                    value={form.last_name}
+                                    onChangeText={last_name => setForm({ ...form, last_name })}
+                                    autoCorrect={false}
+                                    keyboardType='default'
+                                />
+                                <Text>E-mail</Text>
+                                <TextInput
+                                    placeholder='E-mail'
+                                    style={styles.inputControl}
+                                    value={form.email}
+                                    onChangeText={email => setForm({ ...form, email })}
+                                    autoCapitalize='none'
+                                    autoCorrect={false}
+                                    keyboardType='email-address'
+                                />
+                                <Text>Username</Text>
+                                <TextInput
+                                    placeholder='Username'
+                                    style={styles.inputControl}
+                                    value={form.username}
+                                    onChangeText={username => setForm({ ...form, username })}
+                                    autoCapitalize='none'
+                                    autoCorrect={false}
+                                    keyboardType='default'
+                                />
+                                <Text>Password</Text>
+                                <TextInput
+                                    placeholder='Password'
+                                    style={styles.inputControl}
+                                    value={form.password}
+                                    onChangeText={password => setForm({ ...form, password })}
+                                    autoCapitalize='none'
+                                    autoCorrect={false}
+                                    keyboardType='default'
+                                    secureTextEntry={true}
+                                />
+                                <Text>Verify Password</Text>
+                                <TextInput
+                                    placeholder='Verify Password'
+                                    style={styles.inputControl}
+                                    value={form.verify_password}
+                                    onChangeText={handleVerifyPasswordChange}
+                                    autoCapitalize='none'
+                                    autoCorrect={false}
+                                    keyboardType='default'
+                                    secureTextEntry={true}
+                                />
+                            </View>
 
-                        {errorMessage ? (
-                            <Text style={styles.errorMessage}>{errorMessage}</Text>
-                        ) : null}
+                            {errorMessage ? (
+                                <Text style={styles.errorMessage}>{errorMessage}</Text>
+                            ) : null}
 
-                        <View style={styles.buttonContainer}>
-                            <View style={styles.continueButton}>
-                                <TouchableOpacity style={styles.button} onPress={handleContinue}>
-                                    <Text style={styles.continueButtonText}>Continue</Text>
-                                </TouchableOpacity>
+                            <View style={styles.buttonContainer}>
+                                <View style={styles.continueButton}>
+                                    <TouchableOpacity style={styles.button} onPress={handleContinue}>
+                                        <Text style={styles.continueButtonText}>Continue</Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
                         </View>
-                    </View>
-                </SafeAreaView>
-            </ScrollView>
-        </KeyboardAvoidingView>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+    scrollContainer: {
+        flexGrow: 1, 
+        justifyContent: 'center',  
+    },
     backButton: {
-        flex: 1,
         paddingTop: 50,
-        justifyContent: 'flex-start',
         height: 30,
         width: 30,
         color: colors.secondary,
@@ -171,10 +179,11 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         paddingLeft: 5,
         borderRadius: 5,
+        marginBottom: 10,  
         color: colors.primary,
     },
     formContainer: {
-        marginTop: 80
+        marginTop: 20 
     },
     buttonContainer: {
         marginBottom: 80
@@ -189,7 +198,6 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         alignSelf: 'center',
-
     },
     button: {
         backgroundColor: colors.secondary,
@@ -198,7 +206,7 @@ const styles = StyleSheet.create({
         borderRadius: 10
     },
     errorMessage: {
-        color: colors.tertiery,
+        color: colors.tertiary,
         textAlign: 'center',
         marginTop: 10,
     }
