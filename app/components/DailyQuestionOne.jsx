74d98063cfc60a05e6dc934cf41e800904566f0a
Brendan O'Brien <bobrien@pursuit.org>
2024-10-06 18:35:31 -0400

    Revert "gets something on the page"
    This reverts commit 59940568dfbe62a3e0532001d8f98887769a90d2.

---
+++ app/components/DailyQuestionOne.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { 
+    StyleSheet, 
+    Pressable, 
+    View
+} from 'react-native';
+
+import { 
+        PaperProvider, 
+        Card, 
+        RadioButton 
+} from 'react-native-paper';
+    
+import colors from '../config/colors';
+
+const dailyQuestionCard = ()
+
+
+export default DailyQuestionOne; 
--- app/screens/DailyCheckInOne.jsx
+++ app/screens/DailyCheckInOne.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import MainButton from '../components/MainButton';
 import PrimarySubmitButton from '../components/PrimarySubmitButton';
-import DailyQuestionOneCard from '../components/DailyQuestionOneCard';
 
 
 
@@ -11,28 +10,25 @@ const DailyCheckInOne = () => {
   return (
     <SafeAreaView style={styles.container}>
 
-      <View style={styles.headersContainer}>
-        <Text style={styles.title}>Daily Check-In</Text>
-        <Text style={styles.subHeader}>How's Your Mood Today?</Text>
-      </View>
+        <View style={styles.headersContainer}>
+            <Text style={styles.title}>Daily Check-In</Text> 
+            <Text style={styles.subHeader}>How's Your Mood Today?</Text>
+        </View>
 
-      <View style={styles.buttonContainer}>
+        <View style={styles.buttonContainer}>
 
-        <MainButton buttonText="1. Overwhelmed" />
-        <MainButton buttonText="2. Stressed" />
-        <MainButton buttonText="3. Nuetral" />
-        <MainButton buttonText="4. Content" />
-        <MainButton buttonText="5. Peaceful" />
+            <MainButton buttonText="1. Overwhelmed" />
+            <MainButton buttonText="2. Stressed" />
+            <MainButton buttonText="3. Nuetral" />
+            <MainButton buttonText="4. Content" />
+            <MainButton buttonText="5. Peaceful" />
+       
+        </View>
 
-      </View>
-      <View>
-        <DailyQuestionOneCard Text="Sample Question 1" />
-      </View>
-
-      <View style={styles.continueButton}>
-        <PrimarySubmitButton buttonText="Continue" />
-      </View>
-
+        <View style={styles.continueButton}>
+            <PrimarySubmitButton   buttonText="Continue" />
+        </View>
+        
 
     </SafeAreaView>
 
@@ -41,34 +37,34 @@ const DailyCheckInOne = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 16,
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 16,
   },
-  headersContainer: {
-    marginTop: 32,
-    marginBottom: 16,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
+    headersContainer: {
+        marginTop: 32,
+        marginBottom: 16,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
   title: {
     fontSize: 40,
-
+   
   },
-  subHeader: {
-    fontSize: 20,
-    marginTop: 16,
-  },
-  buttonContainer: {
-    flexGrow: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  continueButton: {
-    marginBottom: 32,
-  }
+    subHeader: {
+        fontSize: 20,
+        marginTop: 16,
+    },
+    buttonContainer: {
+        flexGrow: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        marginBottom: 16,
+    },
+    continueButton: {
+        marginBottom: 32,
+    }
 });
 
 export default DailyCheckInOne;
