ab190f0449f9f89b481ec948b72bb15635a72ad5
MiloCo86 <cpaez@pursuit.org>
2024-10-07 11:31:40 -0400

    fixing routing

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,9 @@ import { Text, View } from "react-native";
 
 
 import Login from "./screens/Login";
-// import DailyCheckInOne from "./screens/DailyCheckInOne";
-import DailyCheckInTwo from "./screens/DailyCheckInTwo";
+
 
 
 export default function Index() {
-  return <DailyCheckInTwo />;
+  return <Login />;
 }
