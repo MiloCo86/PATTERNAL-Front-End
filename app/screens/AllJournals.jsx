8f00da31dba6eaf8f6f2de7cd08e87ff494ea501
MiloCo86 <cpaez@pursuit.org>
2024-10-09 11:24:49 -0400

    Journal MiniCard component created, add some styling to all journal screens

--- app/screens/AllJournals.jsx
+++ app/screens/AllJournals.jsx
@@ -5,8 +5,9 @@ import { Divider } from 'react-native-paper'
 //Config files
 import colors from '../config/colors'
 //Components
-import PrimarySubmitButton from '../components/PrimarySubmitButton'
+import JournalMiniCard from '../components/JournalMiniCard'
 import ButtonList from '../components/ButtonList'
+
 
 
 const AllJournals = () => {
@@ -20,7 +21,7 @@ const AllJournals = () => {
     <SafeAreaView style={styles.journalsContainer}>
         
         <View style={styles.headersContainer}>
-            <Image source={require('../assets/logo.png')} style={styles.logo}   />
+            <Image source={require('../assets/logo-full.png')} style={styles.logo}   />
             <Text style={styles.headerText}>All Journals</Text>
         </View>
         <View style={styles.buttonsContainer}>
@@ -28,12 +29,11 @@ const AllJournals = () => {
         </View>
         <Divider />
         <View style={styles.journalList}>
-            <Text>Journal 1</Text>
-            <Text>Journal 2</Text>
-            <Text>Journal 3</Text>
+            <JournalMiniCard mood={1} note={'Today was not a good day, financial issues are generating problems with Debi'}/>
+            <JournalMiniCard mood={3} note={'It was a regular day, I was not able to see my daughter, but I call her in the morning'}/>
+            <JournalMiniCard mood={5} note={'We had a great time at six flag today!!!'}/>
+            <JournalMiniCard mood={2} note={'I am feeling a little bit down today, I am not sure why'}/>
         </View>
-
-
 
     </SafeAreaView>
   )
@@ -55,11 +55,22 @@ const styles = StyleSheet.create({
         marginTop: 50,
     },
     logo: {
-        width: 44,
-        height: 44,
-        marginTop: 8
+        width: 144,
+        height: 144
     },
     buttonsContainer: {
+        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 20
+    },
+    headerText: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: 'bold',
+        color: colors.primary
+    },
+    journalList: {
         width: '100%',
         justifyContent: 'flex-start',
         alignItems: 'center',
