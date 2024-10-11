a4f0120f73a224847cca2a02193656116a7ffa5c
Brendan O'Brien <bobrien@pursuit.org>
2024-10-11 14:15:51 -0400

    finishes Journal Note Component

--- app/components/JournalNote.jsx
+++ app/components/JournalNote.jsx
@@ -7,45 +7,67 @@ import colors from '../config/colors'
 const JournalNote = () => {
 
   return (
-    // <View>
-    <Card style={styles.noteCard}>
+    <Card style={styles.noteCardContainer}>
       <Card.Content>
         <Text style={styles.noteDate}>Today's Date</Text>
-        <View style={styles.avatarTitle}>
-          <Image source={require('../assets/generic-avatar.png')} />
-          <Text>This is the title</Text>
+        <View style={styles.noteCardHeader}>
+          <Image source={require('../assets/generic-avatar.png')} style={styles.avatar} />
+          <Text style={styles.avatarTitle}>This is the title</Text>
         </View>
-        <Text style={styles.noteCardQuestion}>This is why I typed today</Text>
-        <Card.Actions>
+        <Text style={styles.noteCardQuestion}>This is why I typed today. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsum rem fugit facere ipsa quam vel aspernatur quis minima expedita quas, culpa, harum inventore quia delectus eveniet? Rerum, at quis!</Text>
+        <Card.Actions style={styles.dailyCheckInStatus}>
           <Image source={require('../assets/journalcheck-incomplete.png')} />
-          <Image source={require('../assets/journalcheck-incomplete.png')} />
-          <Image source={require('../assets/journalcheck-incomplete.png')} />
+          <Image source={require('../assets/journalcheck-completed.png')} />
           <Image source={require('../assets/journalcheck-completed.png')} />
         </Card.Actions>
       </Card.Content>
     </Card>
-    // </View>
   )
 
 }
 
 const styles = StyleSheet.create({
 
+  noteCardContainer: {
+    justifyContent: 'space-between',
+  },
   noteCardQuestion: {
-    color: colors.primary,
+    color: colors.altSecondary,
     alignSelf: 'center',
+    justifyContent: 'space-between',
   },
   noteDate: {
     textAlign: 'right',
+    fontSize: 20,
+    color: 'black',
+    fontFamily: 'Roboto',
+    fontWeight: '700',
+    lineHeight: 30,
+  },
+  noteCardHeader: {
+    flexDirection: 'row',
+    paddingBottom: 20,
+    margin: 10,
+    alignItems: 'center',
   },
   noteCard: {
     height: 377,
     width: 294,
     padding: 0,
   },
+  avatar: {
+    marginRight: 20,
+  },
   avatarTitle: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    fontSize: 16,
+    fontFamily: 'Roboto',
+    fontWeight: '700',
+    lineHeight: 30
+  },
+  dailyCheckInStatus: {
+    alignSelf: 'center',
+    paddingBottom: 50,
+    paddingTop: 30,
   }
 
 });
