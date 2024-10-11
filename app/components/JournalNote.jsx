0d932c5c0f9894c5665e37e5f2828b59c3d6d74a
Brendan O'Brien <bobrien@pursuit.org>
2024-10-11 14:28:35 -0400

    changes color from altSecondary to Primary in noteCardContentl

--- app/components/JournalNote.jsx
+++ app/components/JournalNote.jsx
@@ -14,7 +14,7 @@ const JournalNote = () => {
           <Image source={require('../assets/generic-avatar.png')} style={styles.avatar} />
           <Text style={styles.avatarTitle}>This is the title</Text>
         </View>
-        <Text style={styles.noteCardQuestion}>This is why I typed today. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsum rem fugit facere ipsa quam vel aspernatur quis minima expedita quas, culpa, harum inventore quia delectus eveniet? Rerum, at quis!</Text>
+        <Text style={styles.noteCardContent}>This is why I typed today. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsum rem fugit facere ipsa quam vel aspernatur quis minima expedita quas, culpa, harum inventore quia delectus eveniet? Rerum, at quis!</Text>
         <Card.Actions style={styles.dailyCheckInStatus}>
           <Image source={require('../assets/journalcheck-incomplete.png')} />
           <Image source={require('../assets/journalcheck-completed.png')} />
@@ -31,8 +31,8 @@ const styles = StyleSheet.create({
   noteCardContainer: {
     justifyContent: 'space-between',
   },
-  noteCardQuestion: {
-    color: colors.altSecondary,
+  noteCardContent: {
+    color: colors.primary,
     alignSelf: 'center',
     justifyContent: 'space-between',
   },
