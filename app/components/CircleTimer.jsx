75b48bed5f1ba126e91fb70ec2866b84eb6cd63a
Brendan O'Brien <bobrien@pursuit.org>
2024-10-13 17:59:04 -0400

    not sure why getting undefined saving progress for later

--- app/components/CircleTimer.jsx
+++ app/components/CircleTimer.jsx
@@ -1,27 +1,41 @@
 import * as React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
 import { CountDownCircleTimer } from 'react-native-countdown-circle-timer';
 
 import colors from '../config/colors';
 
-const CircleTimer = () => {
+export default CircleTimer = () => {
+    const [isPlaying, setIsPlaying] = React.useState(true)
+
     return (
-        <CountDownCircleTimer
-            isPlaying
-            duration={10}
-            colors={[
-                [colors.primary, 0.4],
-                [colors.secondary, 0.4],
-                [colors.tertiary, 0.2],
-            ]}
-        >
-            {({ remainingTime, animatedColor }) => (
-                <Text style={{ color: animatedColor, fontSize: 40 }}>
-                    {remainingTime}
-                </Text>
-            )}
-        </CountDownCircleTimer>
+        <View style={styles.container}>
+            <CountDownCircleTimer
+                isPlaying={isPlaying}
+                duration={10}
+                colors={[colors.primary, colors.secondary, colors.tertiery]}
+                colorsTime={[10, 6, 3]}
+                onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+                updateInterval={1}
+            >
+                {({ remainingTime, color }) => (
+                    <Text style={{ color, fontSize: 40 }}>
+                        {remainingTime}
+                    </Text>
+                )}
+            </CountDownCircleTimer>
+            <Button title="Toggle Playing" onPress={() => setIsPlaying(prev => !prev)} />
+        </View>
     );
 }
 
-export default CircleTimer;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    }
+});
