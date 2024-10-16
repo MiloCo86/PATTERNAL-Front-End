d2db76c0e7dcc5f46dff69b707eb850fcc41158c
MoinulAx <moinulkhan347@gmail.com>
2024-10-16 00:22:35 -0400

    Daily Checking Crud complete

--- app/checkin/DailyCheckInTwo.jsx
+++ app/checkin/DailyCheckInTwo.jsx
@@ -1,40 +1,23 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { View, Text, StyleSheet,Image, SafeAreaView, FlatList, Pressable } from 'react-native';
-
-
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, SafeAreaView, FlatList, Pressable } from 'react-native';
 import PrimarySubmitButton from '../components/PrimarySubmitButton';
 import PrimaryCard from '../components/PrimaryCard';
 import TextInputBox from '../components/TextInputBox';
-
 import colors from '../config/colors';
-
 import { router, useLocalSearchParams } from 'expo-router';
-
-
-// import helper functions
+import axios from 'axios';
+import { API_URL } from '@env';
+// Import helper functions
 import { getCheckInQuestions } from '../config/helperFunctions';
 
-// const tempData = [ 
-//     { questionNum: '1', questionId: '12', text: "Question#1: Will be Sourced based on user's mood." },
-//     { questionNum: '2', questionId: '33', text: "Question#2: Will be Sourced based on user's mood." },
-//     { questionNum: '3', questionId: '03', text: "Question#3: Will be Sourced based on user's mood." },
-
-// ];
-
-
-// DailyCheckInTwo will be the second screen in the Daily Check-In flow. It will display a list of questions that the user will answer.
 const DailyCheckInTwo = () => {
-    //string with the user's id and mood obtained from the previous screen using useLocalSearchParams
     const { idAndMood } = useLocalSearchParams();
-    //splitting the string to get the user's id and mood
-    const id = idAndMood.split(',')[0]
-    const mood = +idAndMood.split(',')[1]
-    const entryId = idAndMood.split(',')[2]
+    const id = idAndMood.split(',')[0];
+    const mood = +idAndMood.split(',')[1];
+    const entryId = idAndMood.split(',')[2];
 
-    const [tempData,setTempData] =  useState(getCheckInQuestions(mood))
+    const [tempData, setTempData] = useState(getCheckInQuestions(mood));
     const [errorMessage, setErrorMessage] = useState('');
-    //checkinData will store the user's responses to the questions and the user's journal entry
     const [checkinData, setCheckinData] = useState({
         userId: id,
         mood: mood,
@@ -43,112 +26,126 @@ const DailyCheckInTwo = () => {
         question3: { id: '', answer: '' },
         journal: ''
     });
-    // const backend = {
-    //     "question_text": "Did you enjoy anything specific today?",
-    //     "mood_level": "5",
-    //     "answer": true,
-    //     "content_section": "Daily Check-in"
-    // }
-
-    // Query to note table "note": "Need to keep up with the morning run routine."
-    //
 
     const [journalText, setJournalText] = useState('');
-    
-    
-    
+
     const handleBack = () => {
         return router.push({
             pathname: 'checkin/DailyCheckInOne',
             params: { userId: id }
-            });
-    }
-    // getResponse will update the checkinData state with the user's response to the question
+        });
+    };
+
     const getResponse = (cardData) => {
         setErrorMessage('');
-        if (cardData.questionNum === '1') {
-            setCheckinData({ ...checkinData, question1: { id: cardData.questionId, answer: cardData.response } });
-        } else if (cardData.questionNum === '2') {
-            setCheckinData({ ...checkinData, question2: { id: cardData.questionId, answer: cardData.response } });
-        } else if (cardData.questionNum === '3') {
-            setCheckinData({ ...checkinData, question3: { id: cardData.questionId, answer: cardData.response } });
-        }
-    }
+        setCheckinData(prevState => ({
+            ...prevState,
+            [`question${cardData.questionNum}`]: { id: cardData.questionId, answer: cardData.response }
+        }));
+    };
 
-    const handleFinish = () => {
-        
-        if (checkinData.question1.answer === '' || checkinData.question2.answer === '' || checkinData.question3.answer === '') {
+    const handleFinish = async () => {
+        if (!checkinData.question1.answer || !checkinData.question2.answer || !checkinData.question3.answer) {
             setErrorMessage('Please answer all questions.');
             return;
-        }else{
-            console.log('Checkin Data:', checkinData)
-            return router.push({
+        }
+
+        try {
+            // Create journal questions using the specified backend route
+            await Promise.all([
+                axios.post(`${API_URL}/users/${id}/journal-entries/${entryId}/questions/`, {
+                    question_text: tempData[0].text,
+                    content_section: 'Daily Check-in',
+                    mood_level: mood.toString(),
+                    answer: checkinData.question1.answer
+                }),
+                axios.post(`${API_URL}/users/${id}/journal-entries/${entryId}/questions/`, {
+                    question_text: tempData[1].text,
+                    content_section: 'Daily Check-in',
+                    mood_level: mood.toString(),
+                    answer: checkinData.question2.answer
+                }),
+                axios.post(`${API_URL}/users/${id}/journal-entries/${entryId}/questions/`, {
+                    question_text: tempData[2].text,
+                    content_section: 'Daily Check-in',
+                    mood_level: mood.toString(),
+                    answer: checkinData.question3.answer
+                })
+            ]);
+
+            // Create a note with the journal text
+            if (journalText.trim()) {
+                await axios.post(`${API_URL}/users/${id}/journal-entries/${entryId}/notes/`, {
+                    entry_id: entryId,
+                    note: journalText
+                });
+            }
+
+            console.log('Checkin Data:', checkinData);
+            router.push({
                 pathname: 'screens/Home',
                 params: { userId: id }
-            })
-        }      
-    }
+            });
+        } catch (error) {
+            console.error('Error creating journal questions or note:', error);
+            setErrorMessage('Failed to create journal questions or note.');
+        }
+    };
 
     useEffect(() => {
-        console.log('Checkin Data:', checkinData)
-    }, [checkinData])
+        console.log('Checkin Data:', checkinData);
+    }, [checkinData]);
 
     useEffect(() => {
-        console.log(tempData)
-    }, [tempData])
+        console.log(tempData);
+    }, [tempData]);
 
     useEffect(() => {
-        setCheckinData({ ...checkinData, journal: journalText });
-    }, [journalText])
+        setCheckinData(prevState => ({ ...prevState, journal: journalText }));
+    }, [journalText]);
 
-  return (
-    <SafeAreaView style={styles.container}>
-        <Pressable onPress={handleBack} style={styles.arrowContainer} >
-            <Image source={require('../assets/back-arrow.png')} style={styles.backArrow} />
-        </Pressable>
-        
-        <Image source={require('../assets/logo.png')} style={styles.logo} />
+    return (
+        <SafeAreaView style={styles.container}>
+            <Pressable onPress={handleBack} style={styles.arrowContainer}>
+                <Image source={require('../assets/back-arrow.png')} style={styles.backArrow} />
+            </Pressable>
 
-        <View style={styles.headersContainer}>
-            <Text style={styles.headerText}>Daily Check-In</Text> 
-            <Text style={styles.subHeader}>Mood Questionaire</Text>
-        </View>
-                
-            {/* React native component that can render a list of items in a horizontal direction. Checkout Flatlist Props in the React Native Docs
-            */}
-        <FlatList
-            data={tempData}
-            renderItem={({ item }) => (
-                <View style={styles.cardContainer}>
-                <PrimaryCard CardText={item.text} questionNum={item.questionNum} questionId={item.questionId} getResponse={getResponse}/>
-                </View>
-            )}
-            keyExtractor={item => item.questionNum}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.flatListContent}
-        />
+            <Image source={require('../assets/logo.png')} style={styles.logo} />
 
-        <View style={styles.textInputContainer}>
-            <TextInputBox placeholder="Daily Log" text={journalText} setText={setJournalText}/>    
-        </View>
+            <View style={styles.headersContainer}>
+                <Text style={styles.headerText}>Daily Check-In</Text>
+                <Text style={styles.subHeader}>Mood Questionnaire</Text>
+            </View>
 
-        {errorMessage ? (
-          <Text style={styles.errorText}>{errorMessage}</Text>
-        ) : null}
+            <FlatList
+                data={tempData}
+                renderItem={({ item }) => (
+                    <View style={styles.cardContainer}>
+                        <PrimaryCard CardText={item.text} questionNum={item.questionNum} questionId={item.questionId} getResponse={getResponse} />
+                    </View>
+                )}
+                keyExtractor={item => item.questionNum}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.flatListContent}
+            />
 
-        <View style={styles.finishButton}>
-            <PrimarySubmitButton   buttonText="Finish" onPress={handleFinish}/>
-        </View>
-    
+            <View style={styles.textInputContainer}>
+                <TextInputBox placeholder="Daily Log" text={journalText} setText={setJournalText} />
+            </View>
 
-    </SafeAreaView>
+            {errorMessage ? (
+                <Text style={styles.errorText}>{errorMessage}</Text>
+            ) : null}
 
-  );
-}
+            <View style={styles.finishButton}>
+                <PrimarySubmitButton buttonText="Finish" onPress={handleFinish} />
+            </View>
+        </SafeAreaView>
+    );
+};
 
-const styles = StyleSheet.create({ 
-    // whole screen container
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'space-between',
@@ -156,73 +153,54 @@ const styles = StyleSheet.create({
         backgroundColor: colors.altSecondary,
         width: '100%',
     },
-    //temp logo - want the one in with our primary color
-    logo: { 
+    logo: {
         width: 44,
         height: 44,
         marginTop: 8,
         color: colors.secondary
     },
-    // back arrow container
     arrowContainer: {
         alignSelf: 'flex-start',
         marginTop: 25,
         marginLeft: 25,
     },
-    // back arrow
     backArrow: {
-        
         width: 26,
         height: 26,
         marginBottom: -32,
-       
     },
-    
-    // header and subheader
     headersContainer: {
         justifyContent: 'flex-end',
         alignItems: 'center',
         marginTop: 16,
     },
-  
-    // Daily Check-In header
     headerText: {
-    fontSize: 28,
-    marginTop: 16,
-    fontWeight: 'bold',
+        fontSize: 28,
+        marginTop: 16,
+        fontWeight: 'bold',
     },
     subHeader: {
         fontSize: 12,
     },
-
-    // flatlist container
     cardContainer: {
-       marginHorizontal: 16,
-       marginTop: 16,
+        marginHorizontal: 16,
+        marginTop: 16,
     },
-
-    // text on card within the carousel
     flatListContent: {
         paddingHorizontal: 16,
     },
-
-    //journal text entry
     textInputContainer: {
-       top: 32,
+        top: 32,
         justifyContent: 'center',
         alignItems: 'center',
     },
-
-    // finish button
     finishButton: {
         marginBottom: 64,
     },
-    // error message
     errorText: {
         color: 'red',
         marginBottom: 16,
     },
-
 });
 
 export default DailyCheckInTwo;
