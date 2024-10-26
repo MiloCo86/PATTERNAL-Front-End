9c1d45b998c082184df76a4274afa673ffcd74dc
Brendan O'Brien <bobrien@pursuit.org>
2024-10-26 16:14:32 -0400

    changes Breather to lower case b and moves text to new line

--- app/screens/Meditation.jsx
+++ app/screens/Meditation.jsx
@@ -65,7 +65,7 @@ const Meditation = () => {
         <View style={styles.container}>
 
             <TopBar title="Meditation" />
-            <Text style={styles.instructions}><Text style={{ fontWeight: '700' }}>Take a Breather!</Text> Please take a few minutes to relax and think about the children. Your streaks will be tallied daily. Let's see how you do.</Text>
+            <Text style={styles.instructions}>{'\n'}<Text style={{ fontWeight: '700' }}>Take a breather!</Text> {'\n\n'}Please take a few minutes to relax and think about the children. Your streaks will be tallied daily. Let's see how you do.</Text>
 
             <MeditationTimer updateUser={handleMeditationEnd} />
 
@@ -94,6 +94,7 @@ const styles = StyleSheet.create({
         textAlign: 'left',
         marginBottom: '10%',
         color: colors.primary,
+        paddingBottom: 0
     },
     streakContainer: {
         height: 100,
@@ -105,7 +106,7 @@ const styles = StyleSheet.create({
     streakText: {
         fontSize: 25,
         color: colors.primary,
-    },
+    }
 })
 
 export default Meditation;
